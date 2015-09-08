1b9580b587349c42611f267c93d6f81ec8bf0413
刘峰 <admin@hainuo.info>
2015-09-08 16:50:42 +0800

    update default.js

--- config/default.js
+++ config/default.js
@@ -12,18 +12,18 @@ module.exports = {
   debug: true, //useful for seeing some logs in the browser console
   permalinkStructure:'/:year/:month/:day/:title/', //you can also use /:title/ or '/:year/:month/:title/'
   postPerPage: 3, // number of posts per page
-  siteUrl: 'http://localhost:3000', // the url of your website
+  siteUrl: 'http://morpheus-reactblog-d5a6.coding.io', // the url of your website
   useSSL: false, // if true it redirects all incoming requests to the https url
   siteTitle: 'Morpheus',
   theme: 'blablabla', // currently used theme
   siteDescription: '- say hi to the next generation web publishing platform -',
-  port: 3000,
-  ip: '127.0.0.1',
+  port: 80,
+  ip: '0.0.0.0',
   authors: {
-    'youreamail@yourwebsite.something': {
+    'admin@hainuo.info': {
       meta: 'I really like to write :)'
     }
   },
   googleAnalytics : 'UA-XXXXX', //your google analytics tracking code
-  disqusComments : '' //your disqus shortname
+  disqusComments : 'hainuo' //your disqus shortname
 };
